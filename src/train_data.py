fa6392646a31a3d7bdabc6d0cd299cbab37be253
hallowf <alexandre.cortegaca@gmail.com>
2019-05-18 02:24:16 +0100

    Passed network type as parameter

--- src/train_data.py
+++ src/train_data.py
@@ -6,12 +6,14 @@ from network.network import CNN
 class Trainer(object):
     """docstring for Trainer."""
 
-    def __init__(self):
+    def __init__(self,network_type="unknown", optimizer="Adam"):
         super(Trainer, self).__init__()
+        self.network = network_type
+        self.optimizer = optimizer
 
 
     def run_neural_network(self):
-        cnn = CNN(set=2, network_type="VGG")
+        cnn = CNN(set=2, network_type=self.network)
         cnn.start()
 
 
@@ -21,4 +23,4 @@ class Trainer(object):
 
 if __name__ == '__main__':
     args = None
-    Trainer().run_neural_network()
+    Trainer("CIFAR10").run_neural_network()
