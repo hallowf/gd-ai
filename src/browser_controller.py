b89cdf51086d8f5ac17d9eaa6c1b9695155ccb2d
hallowf <alexandre.cortegaca@gmail.com>
2019-06-03 16:51:05 +0100

    Fixed some minor issues with browser controller

--- src/browser_controller.py
+++ src/browser_controller.py
@@ -6,31 +6,33 @@ from selenium.common.exceptions import SessionNotCreatedException
 
 class BController(object):
 
-    def __init__(self, version="75"):
+    def __init__(self, version):
         self.driver_version = version
         chrome_options = Options()
         chrome_options.add_argument("--disable-extensions")
         chrome_options.add_argument("--disable-background-networking")
         self.chrome_options = chrome_options
-        self.test_driver()
-        if not self.driver_connected:
-            sys.stdout.write("Erro de driver. Nenhuma versão compativel do chrome encontrada\n")
-            sys.exit(1)
-        else:
+        has_driver = self.test_driver()
+        if has_driver:
             sys.stdout.write("Browser iniciado\n")
 
     def test_driver(self):
+        print("d_version", self.driver_version)
         try:
-            self.driver = webdriver.Chrome("drivers/%s" % (self.driver_version), options=self.chrome_options)
-            time.sleep(2)
+            self.driver = webdriver.Chrome("drivers/%i" % (self.driver_version), options=self.chrome_options)
             self.driver.get("chrome://dino")
-            self.driver_connected = True
+            time.sleep(2)
+            print("found driver")
+            return True
         except (Exception, SessionNotCreatedException) as e:
-            self.driver = None
-            self.driver_version = "74" if self.driver_version == "75" else "73"
-            if self.driver_version > "75" or self.driver_version < "73":
+            print("d_version exception", self.driver_version)
+            print(e)
+            self.driver_version -= 1
+            if self.driver_version < 70:
+                sys.stdout.write("Erro de driver. Nenhuma versão compativel do chrome encontrada\n")
                 return False
-            self.test_driver()
+            else:
+                self.test_driver()
 
     def get_driver(self):
         return self.driver
